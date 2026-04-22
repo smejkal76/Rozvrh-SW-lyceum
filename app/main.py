68fa5532c40605cf8892fdb07b636f87334c07f2
schejbalm <matyas.schejbal@waldorf.pb.cz>
2026-04-22 12:08:55 +0200

    12 - lze upravovat ušechny konolonky: Třída, Učitel, Učebna, Hodina, Počet hodin, Zaměření ,Půlený

--- app/main.py
+++ app/main.py
@@ -2,10 +2,12 @@
 from fastapi.staticfiles import StaticFiles
 
 from app.web.timetable import router as timetable_router
+from app.web.subjects import router as subjects_router  # ← PŘIDEJ TUTO ŘÁDKU
 
 app = FastAPI(title="Rozvrh SŠ")
 
 app.include_router(timetable_router)
+app.include_router(subjects_router)  # ← PŘIDEJ TUTO ŘÁDKU
 
 # statické soubory (CSS)
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
@@ -13,5 +15,4 @@ app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 @app.get("/")
 def root():
-    # jednoduchý redirect by šel taky, ale nechám to minimalisticky:
     return {"ok": True, "open": "/view/day?den=Po"}
