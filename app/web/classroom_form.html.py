16cbcccf9b6d0dd36d9a4e01e84ba709822f20b8
bezuchl <lukas.bezuch@waldorf.pb.cz>
2026-04-29 12:29:58 +0200

    Editace učeben

--- app/web/classroom_form.html.py
+++
@@ -1,9 +0,0 @@
-{% extends 'base.html' %}
-{% block content %}
-<h1>Editace učebny</h1>
-<form method="post" action="/classrooms/{{ ucebna.id }}">
-  <input name="nazev" value="{{ ucebna.nazev }}" required>
-  <button type="submit">Uložit</button>
-  <a href="/view/classrooms">Zrušit</a>
-</form>
-{% endblock %}
